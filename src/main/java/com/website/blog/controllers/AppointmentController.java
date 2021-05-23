4f521f6ad64280f2121237bb9242f4c9075e267d
Danil <danil-sem2001@mail.ru>
2021-05-23 23:35:19 +0300

    Добавление базы данных и формы для её заполнения

--- src/main/java/com/website/blog/controllers/AppointmentController.java
+++ src/main/java/com/website/blog/controllers/AppointmentController.java
@@ -1,13 +1,38 @@
 package com.website.blog.controllers;
 
+import com.website.blog.models.appointment;
+import com.website.blog.repo.appointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppointmentController {
-    @GetMapping("/appointment")
+
+    @Autowired
+    private appointmentRepository appointmentRepository;
+
+    @GetMapping("/view")
     public String appointmentMain(Model model) {
+        Iterable<appointment> appointments = appointmentRepository.findAll();
+        model.addAttribute("appointments", appointments);
+        return "viewingAppointments";
+    }
+
+    @GetMapping("/appointment")
+    public String appointmentAdd(Model model) {
         return "appointment-main";
+    }
+
+    @PostMapping("/appointment")
+    public String addedAppointment(String date, @RequestParam String doctor, @RequestParam String time,
+                                   @RequestParam String telephone, @RequestParam String status, Model model) {
+        appointment appointment = new appointment(date, doctor, time, telephone, status);
+        appointmentRepository.save(appointment);
+        return "redirect:/view";
     }
 }
